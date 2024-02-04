57fffd30f7a0776ff562fff4460dc9219a335aef
Sara S Camilleri <ssancam391@g.educaand.es>
2024-02-04 18:44:41 +0100

    completo_sinDocumentacion

--- src/Compra.kt
+++ src/Compra.kt
@@ -1,9 +1,23 @@
 class Compra(cliente: String, dia: Int, monto: Double, direccion: String) {
 
-    var cliente: String = ""
-        get() = cliente
+    var cliente: String = cliente
         set(value) {
             require(cliente.isNotBlank()) { "El campo 'nombre' no puede quedar en blanco." }
-            field = cliente
+            field = value
         }
+
+    var dia: Int = dia
+        set(value) {
+            require(dia in 1..31){ "Día debe tener un valor entre 1 y 31." }
+            field = value
+        }
+
+
+    var monto: Double = monto
+
+    var direccion: String = direccion
+
+    override fun toString(): String {
+        return ("Cliente: ${cliente}, dia: $dia, monto: $monto, dirección: $direccion ")
+    }
 }
--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,21 @@
 fun main() {
+    val compra1 = Compra("Nuria", 5, 12780.78, "Calle Las Flores 355")
+    val compra2 = Compra("Jorge Russo", 7, 699.0, "Mirasol 218")
+    val compra3 = Compra("Nuria Costa", 7, 532.90, "Calle Las Flores 355")
+    val compra4 = Compra("Julián Rodriguez", 12, 5715.99, "La Mancha 761")
+    val compra5 = Compra("Jorge Russo", 15, 958.0, "Mirasol 218")
+
+    val listadoClientes = listOf(compra1, compra2, compra3, compra4, compra5)
+    val listadoDirecciones = listOf(compra1.direccion,compra2.direccion,compra3.direccion,compra4.direccion,compra5.direccion)
+    println()
+    println("Listado de facturas: \n")
+    for (factura in listadoClientes){
+        println(factura)
+    }
+    println()
+    println("Direcciones de facturación: \n")
+    for (direccion in listadoDirecciones){
+        println(direccion)
+    }
 
 }
